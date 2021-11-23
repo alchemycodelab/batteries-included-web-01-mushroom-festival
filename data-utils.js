25b924f558ec2410db9119b69c53f5d983eaee33
Danny Cairns <dpcairns@gmail.com>
2021-11-23 13:28:14 -0800

    change export

--- data-utils.js
+++ data-utils.js
@@ -1,4 +1,4 @@
-export function findFriendByName(name, friends) {
+export default function findFriendByName(name, friends) {
     // iterate through the friends
     
     // when you find a friend with the correct name, return that friend
